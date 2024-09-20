9c0944875a6e491d37588c67499faaf13c7f922d
MChandanSwarit <chandanswarit2804@gmail.com>
2024-09-20 18:15:09 +0530

    final

---
+++ main.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('form');
+
+function handleFormSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const { email } = Object.fromEntries(formData);
+
+  const emailError = validateEmail(email);
+
+  if (emailError) {
+    const emailErrorElement = form.querySelector('#email-error');
+    const emailErrorColor = document.getElementById('email');
+    emailErrorElement.innerText = emailError;
+    emailErrorColor.classList.add('error-state');
+  } else {
+    const successMessageElement = document.querySelector('.success-message');
+    const container = document.querySelector('.form-container');
+    const emailText = document.querySelector('.success-message .email-text');
+    container.style.display = 'none';
+    successMessageElement.style.display = 'block';
+    emailText.innerText = email;
+    emailErrorColor.classList.remove('error-state');
+  }
+
+  function validateEmail(email) {
+    if (!email) return 'Email required';
+
+    const isEmail = /^\S+@\S+$/g;
+    if (!isEmail.test(email)) {
+      return 'Valid email required';
+    }
+
+    return '';
+  }
+}
+
+form.addEventListener('submit', handleFormSubmit);
